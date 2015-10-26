adae7173c9f5fcab45917590c98e05ede526f1ca
Virupaksha Swamy U M <vswamy@outlook.com>
2015-10-26 00:50:26 -0700

    snapshot2

--- src/main/java/com/vswamy/abtest_client/JClient.java
+++ src/main/java/com/vswamy/abtest_client/JClient.java
@@ -1,10 +1,11 @@
 package com.vswamy.abtest_client;
 
+import java.util.LinkedList;
+import java.util.Map;
+
 import org.apache.thrift.TException;
-import org.apache.thrift.transport.TSSLTransportFactory;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
 import org.apache.thrift.transport.TTransportException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -54,8 +55,8 @@ public class JClient
 
     private void getExperimentStateTest() throws ExperimentNotFoundException, NullOrEmptyException, TException
     {
-    	logger.info("Starting getExperimentState() test!...");
-    	
+        logger.info("Starting getExperimentState() test!...");
+
         int c = 0;
         int t = 0;
         int e = 0;
@@ -77,15 +78,67 @@ public class JClient
         logger.info("e => {}", e);
     }
 
+    private void getExperimentsStateTest() throws NullOrEmptyException, TException
+    {
+        logger.info("Starting getExperimentsState() test!...");
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("experiment1");
+        list.add("experiment2");
+
+        int xc = 0;
+        int xt = 0;
+        int xe = 0;
+
+        int yc = 0;
+        int yt = 0;
+        int ye = 0;
+        long start = System.nanoTime();
+        for (int i = 0; i < 10000; i++)
+        {
+            Map<String, String> output = client.getExperimentsState(list);
+            String x = output.get("experiment1");
+            String y = output.get("experiment2");
+            if(x != null)
+            if (x.compareTo("C") == 0)
+                xc++;
+            else if (x.compareTo("T") == 0)
+                xt++;
+            else
+                xe++;
+
+            if(y != null)
+            if (y.compareTo("C") == 0)
+                yc++;
+            else if (y.compareTo("T") == 0)
+                yt++;
+            else
+                ye++;
+        }
+        long end = System.nanoTime();
+        
+        logger.info("Time taken in milliseconds for 10000 requests => {}", (end - start) / 1000000.0);
+        logger.info("experiment1 t => {}", xt);
+        logger.info("experiment1 c => {}", xc);
+        logger.info("experiment1 e => {}", xe);
+        
+        logger.info("experiment2 t => {}", yt);
+        logger.info("experiment2 c => {}", yc);
+        logger.info("experiment2 e => {}", ye);
+        
+    }
+
     public static void main(String[] args) throws TException
     {
 
         logger.info("client is starting...");
 
         JClient jclient = new JClient();
-        jclient.pingTest();
-        jclient.getExperimentStateTest();
         
+        //jclient.pingTest();
+        
+        //jclient.getExperimentStateTest();
+
+        jclient.getExperimentsStateTest();
         return;
     }
 }
